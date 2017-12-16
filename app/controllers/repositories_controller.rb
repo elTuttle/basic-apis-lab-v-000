dfbafcda42b9d2ead7dbebfc59b5ac1df5737d0b
John Tuttle <thejwt@gmail.com>
2017-12-16 05:16:13 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -8,7 +8,7 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
         req.params['client_id'] = client_id
         req.params['client_secret'] = client_secret
-        req.params['query'] = 'tetris'
+        req.params['q'] = 'tetris'
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
