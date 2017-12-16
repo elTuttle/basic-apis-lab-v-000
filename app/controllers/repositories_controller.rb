ca9f3e9e6a3e8920e92976a488d1aa478479aa0a
John Tuttle <thejwt@gmail.com>
2017-12-16 05:12:48 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
         req.params['client_id'] = client_id
         req.params['client_secret'] = client_secret
         req.params['v'] = '20160201'
