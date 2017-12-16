6ca7a907c4a7c5d0ac868d1a81ae712ec5547975
John Tuttle <thejwt@gmail.com>
2017-12-16 05:27:01 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -12,7 +12,7 @@ class RepositoriesController < ApplicationController
       body = JSON.parse(@resp.body)
       binding.pry
       if @resp.success?
-        @venues = body["items"]
+        @repositories = body["items"]
       end
 
     render 'search'
